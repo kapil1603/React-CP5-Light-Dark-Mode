03d34fe1606da3d31640bebbaf24d7bfb253e96f
kapil1603 <kapilkumar1603@gmail.com>
2023-07-23 17:55:31 +0000

    React-CP5-Light-Dark-Mode

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -1,38 +1,27 @@
 import {Component} from 'react'
+
 import './index.css'
 
 class LightDarkMode extends Component {
-  state = {dark: true}
+  state = {
+    isDarkMode: true,
+  }
 
   onClickButton = () => {
     this.setState(prevState => ({isDarkMode: !prevState.isDarkMode}))
   }
 
-  checkMark = () => {
-    const {dark} = this.state
-    return dark ? 'Dark Mode' : 'Light Mode'
-  }
-
-  colorClass = () => {
-    const {dark} = this.state
-    return dark ? 'Light-Mode' : 'Dark-Mode'
-  }
-
-  headClass = () => {
-    const {dark} = this.state
-    return dark ? 'Dark-Mode' : 'Light-Mode'
-  }
-
   render() {
-    const darkMode = this.checkDark()
+    const {isDarkMode} = this.state
+    const modeClassName = isDarkMode ? 'dark-mode' : 'light-mode'
+    const buttonText = isDarkMode ? 'Light Mode' : 'Dark Mode'
+
     return (
-      <div className="container">
-        <div className={`sub-container ${this.colorClass()}`}>
-          <h1 className={`heading ${this.headClass()}`}>
-            Click To Change Mode
-          </h1>
-          <button className="button" type="button" onClick={this.onClickButton}>
-            {darkMode}
+      <div className="app-container">
+        <div className={`container ${modeClassName}`}>
+          <h1 className="heading">Click To Change Mode</h1>
+          <button type="button" onClick={this.onClickButton} className="button">
+            {buttonText}
           </button>
         </div>
       </div>
